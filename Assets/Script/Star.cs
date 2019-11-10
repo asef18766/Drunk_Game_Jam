cd4cd0ba11b16fa526bfbb3725a271cb5a98ee22
bogay <pojay11523@gmail.com>
2019-11-10 12:54:53 +0800

    fix title animation

---
+++ Assets/Script/Star.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public float speed;
+    private Transform self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        self.Translate(Vector3.right * (speed * Time.deltaTime));
+    }
+}
